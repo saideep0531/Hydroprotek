a24482be9d9f27175bc5a9facc751c5b9955f30e
saideep0531 <79899127+saideep0531@users.noreply.github.com>
2026-02-23 16:38:29 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "client",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
